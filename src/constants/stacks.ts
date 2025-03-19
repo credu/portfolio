6efd668cf51d14be340f5e3fe29dbb67967a44d7
Jesus Mendoza <ccredulidad@gmail.com>
2025-03-19 02:02:06 -0500

    feature: add Stacks component with technology stacks and improve Header styling

---
+++ src/constants/stacks.ts
@@ -0,0 +1,82 @@
+import { type Technology } from "../types/Technology";
+
+export const FRONTEND: Technology[] = [
+    {
+        name: "HTML",
+        image: "https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg"
+    },
+    {
+        name: "CSS",
+        image: "https://www.vectorlogo.zone/logos/w3_css/w3_css-icon~old.svg"
+    },
+    {
+        name: "JavaScript",
+        image: "https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg"
+    },
+    {
+        name: "Astro",
+        image: "https://raw.githubusercontent.com/pheralb/svgl/main/static/library/astro_dark.svg"
+    },
+    {
+        name: "ReactJS",
+        image: "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
+    },
+    {
+        name: "TailwindCSS",
+        image: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
+    }
+];
+
+export const BACKEND: Technology[] = [
+    {
+        name: "Java",
+        image: "https://www.vectorlogo.zone/logos/java/java-icon.svg"
+    },
+    {
+        name: "Spring Boot",
+        image: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg"
+    },
+    {
+        name: "NodeJS",
+        image: "https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg"
+    },
+    {
+        name: "MySQL",
+        image: "https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg"
+    },
+    {
+        name: "PostgreSQL",
+        image: "https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg"
+    },
+    {
+        name: "MongoDB",
+        image: "https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg"
+    }
+];
+
+export const OTHERS: Technology[] = [
+    {
+        name: "Linux",
+        image: "https://www.vectorlogo.zone/logos/linux/linux-icon.svg"
+    },
+    {
+        name: "Git",
+        image: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
+    },
+    {
+        name: "TypeScript",
+        image: "https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"
+    },
+    {
+        name: "Jest",
+        image: "https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
+    },
+    {
+        name: "Docker",
+        image: "https://www.vectorlogo.zone/logos/docker/docker-icon.svg"
+    },
+    {
+        name: "Postman",
+        image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+    }
+];
