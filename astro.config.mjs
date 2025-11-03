2fa68e5de14a0c04dbf6b24a302a99087b5f01ad
Jesus Mendoza <ccredulidad@gmail.com>
2025-11-02 19:12:14 -0500

    feat: add @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,30 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jesus-mendoza.vercel.app",
+
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "en",
   },
+
   trailingSlash: "never",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: "en",
+      locales: {
+        en: "en-US",
+        es: "es-ES",
+      },
+    }
+  })]
 });
